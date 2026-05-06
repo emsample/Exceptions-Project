ab01bfc110a3bc117262fbaf451db2af859617e9
emsample <esamplebox@gmail.com>
2026-05-06 13:49:02 -0400

    Basic Employee information input function, this is where our catch will occur for invalid emplotee numbers.

--- Exceptions Test/main.cpp
+++ Exceptions Test/main.cpp
@@ -5,6 +5,35 @@
 #include "TeamLeader.h"
 using namespace std;
 
+void getEmployeeData(string& name, int& number, string& hireDate)
+{
+    cout << "Enter employee name: ";
+    getline(cin, name);
+
+    while (true)
+    {
+        try
+        {
+            cout << "Enter employee number (0-9999): ";
+            cin >> number;
+            cin.ignore();
+
+            Employee temp;
+            temp.setEmployeeNumber(number);
+
+            break;
+        }
+        catch (Employee::InvalidEmployeeNumber)
+        {
+            cout << "ERROR: Employee number must be between 0 and 9999." << endl;
+            cout << "Please re-enter a valid employee number." << endl;
+        }
+    }
+
+    cout << "Enter hire date: ";
+    getline(cin, hireDate);
+}
+
 int main()
 {
    
